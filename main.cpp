2f1d1c6e753b6928de72b3fe4576bfe0e016a23d
Laszlo Bartok <bartok@nowtech.hu>
2024-08-03 20:20:59 +0200

    misc

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "classic.hpp"
+#include "template_events.hpp"
 #include "template_id.hpp"
 #include "template_states.hpp"
 #include "variadic_states.hpp"
-#include <iostream>
 int main() {
   classic::test();
   template_id::test();
   template_states::test();
   variadic_states::test();
+  template_events::test();
   return 0;
 }
---
+++ template_events.hpp
@@ -0,0 +1,14 @@
+#ifndef STATE_MACHINES_TEMPLATE_EVENTS_HPP
+#define STATE_MACHINES_TEMPLATE_EVENTS_HPP
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <variant>
+
+namespace template_events {
+
+void test() {
+}
+}// namespace template_events
+
+#endif//STATE_MACHINES_TEMPLATE_EVENTS_HPP
