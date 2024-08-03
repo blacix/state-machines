4d40e8afbe41ba867559353a98d4f93de197db81
Laszlo Bartok <bartok@nowtech.hu>
2024-08-03 19:32:42 +0200

    variadic

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "classic.hpp"
-#include "template_events.hpp"
 #include "template_id.hpp"
 #include "template_states.hpp"
+#include "variadic_states.hpp"
 #include <iostream>
 int main() {
   classic::test();
   template_id::test();
   template_states::test();
-  template_events::test();
+  variadic_states::test();
   return 0;
 }
