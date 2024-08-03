98f8e638c2915c8a5d42efefb071e168423c7b76
Laszlo Bartok <bartok@nowtech.hu>
2024-08-03 16:57:34 +0200

    misc

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "classic.hpp"
+#include "template_events.hpp"
 #include "template_id.hpp"
 #include "template_states.hpp"
 #include <iostream>
@@ -6,5 +7,6 @@ int main() {
   classic::test();
   template_id::test();
   template_states::test();
+  template_events::test();
   return 0;
 }
---
+++ template_events.hpp
@@ -0,0 +1,11 @@
+#ifndef STATE_MACHINES_TEMPLATE_EVENTS_HPP
+#define STATE_MACHINES_TEMPLATE_EVENTS_HPP
+#include <iostream>
+
+namespace template_events {
+
+
+void test() {
+}
+}// namespace template_events
+#endif//STATE_MACHINES_TEMPLATE_EVENTS_HPP
