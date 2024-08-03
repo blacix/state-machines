040cad708f2a4288d87d07412b5705985055ef3e
Laszlo Bartok <bartok@nowtech.hu>
2024-08-03 23:12:13 +0200

    another one

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include "template_events.hpp"
 #include "template_id.hpp"
 #include "variadic_flexible.hpp"
+#include "variadic_simple.hpp"
 #include "variadic_states.hpp"
+
 int main() {
   simple::test();
   template_id::test();
+  variadic_simple::test();
   variadic_flexible::test();
   variadic_states::test();
   template_events::test();
